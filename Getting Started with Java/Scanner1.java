8e90aa74ff4664d119633af0e6d46e8b090397ee
Aamir <Aamirsaifi00000@gmail.com>
2021-08-25 18:36:01 +0530

    Create Scanner1.java

---
+++ Getting Started with Java/Scanner1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+
+        /*String str = "This is string";
+
+        String str2 = s.nextLine();
+
+        System.out.println(str);
+        System.out.println(str2);
+        */
+
+        //Simple interest using scanner
+        /*int p = s.nextInt();
+        int r = s.nextInt();
+        int t = s.nextInt();
+
+        int si = p*r*t/100;
+        System.out.println("Simple Interest : " + si);
+    */
+    }
+}
